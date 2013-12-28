79684ed21596f067239353cf010cfdce434c3c10
eccentric devotion <eccentricdevotion@gmail.com>
2013-12-28 17:36:29 +1300

    Add tab completion to commands

--- src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesCommands.java
+++ src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesCommands.java
@@ -1,16 +1,21 @@
 package me.eccentric_nz.gamemodeinventories;
 
+import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
 
-public class GameModeInventoriesCommands implements CommandExecutor {
+public class GameModeInventoriesCommands implements CommandExecutor, TabCompleter {
 
     private final GameModeInventories plugin;
     private final List<String> firstArgs = new ArrayList<String>();
+    private final ImmutableList<String> ROOT_SUBS;
 
     public GameModeInventoriesCommands(GameModeInventories plugin) {
         this.plugin = plugin;
@@ -25,6 +30,8 @@ public class GameModeInventoriesCommands implements CommandExecutor {
         firstArgs.add("save_on_death");
         firstArgs.add("survival_on_world_change");
         firstArgs.add("xp");
+        firstArgs.add("track_creative_place");
+        ROOT_SUBS = ImmutableList.copyOf(firstArgs);
     }
 
     @Override
@@ -50,5 +57,17 @@ public class GameModeInventoriesCommands implements CommandExecutor {
             }
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length <= 1) {
+            return partial(args[0], ROOT_SUBS);
+        }
+        return ImmutableList.of();
+    }
+
+    private List<String> partial(String token, Collection<String> from) {
+        return StringUtil.copyPartialMatches(token, from, new ArrayList<String>(from.size()));
     }
 }
