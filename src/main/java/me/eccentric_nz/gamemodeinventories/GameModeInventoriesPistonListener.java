9950f09a8c24f9b599d86637a0196a232e05a51c
eccentric devotion <eccentricdevotion@gmail.com>
2014-03-11 13:25:45 +1300

    Don't bother checking piston retraction if it's not sticky

--- src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesPistonListener.java
+++ src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesPistonListener.java
@@ -3,10 +3,7 @@
  */
 package me.eccentric_nz.gamemodeinventories;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
@@ -19,17 +16,9 @@ import org.bukkit.event.block.BlockPistonRetractEvent;
 public class GameModeInventoriesPistonListener implements Listener {
 
     private final GameModeInventories plugin;
-    List<BlockFace> faces = new ArrayList<BlockFace>();
 
     public GameModeInventoriesPistonListener(GameModeInventories plugin) {
         this.plugin = plugin;
-        faces.add(BlockFace.UP);
-        faces.add(BlockFace.DOWN);
-        faces.add(BlockFace.NORTH);
-        faces.add(BlockFace.SOUTH);
-        faces.add(BlockFace.EAST);
-        faces.add(BlockFace.WEST);
-
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -50,19 +39,18 @@ public class GameModeInventoriesPistonListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onPistonRetract(BlockPistonRetractEvent event) {
+        if (!event.isSticky()) {
+            return;
+        }
         if (!plugin.getConfig().getBoolean("track_creative_place.enabled")) {
             return;
         }
         if (plugin.getCreativeBlocks().contains(event.getBlock().getLocation().toString())) {
             return;
         }
-        plugin.debug("getDirection() returns: " + event.getDirection().toString());
-        for (BlockFace f : faces) {
-            plugin.debug("block at block face: " + f.toString() + " is " + event.getBlock().getRelative(f).getType().toString());
-        }
         if (plugin.getCreativeBlocks().contains(event.getBlock().getRelative(event.getDirection(), 2).getLocation().toString())) {
             event.setCancelled(true);
-            plugin.debug("Cancelled piston retraction becausethe moved blocks was a CREATIVE placed block");
+            plugin.debug("Cancelled piston retraction because the moved blocks was a CREATIVE placed block");
         }
     }
 }
