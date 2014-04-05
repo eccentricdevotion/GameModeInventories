e65d5df4bc5179326000aa3d2fb8cb1dcbd0d14a
eccentric devotion <eccentricdevotion@gmail.com>
2014-04-05 15:59:54 +1300

    If `no_piston_move` is `true` and the block moved by a piston would break and drop, cancel the event

--- src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesPistonListener.java
+++ src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesPistonListener.java
@@ -3,6 +3,9 @@
  */
 package me.eccentric_nz.gamemodeinventories;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,15 +19,58 @@ import org.bukkit.event.block.BlockPistonRetractEvent;
 public class GameModeInventoriesPistonListener implements Listener {
 
     private final GameModeInventories plugin;
+    private final List<Material> wouldDrop = new ArrayList<Material>();
 
     public GameModeInventoriesPistonListener(GameModeInventories plugin) {
         this.plugin = plugin;
+        wouldDrop.add(Material.BED_BLOCK); // Still Breaks
+        wouldDrop.add(Material.BROWN_MUSHROOM);
+        wouldDrop.add(Material.DIODE_BLOCK_OFF);
+        wouldDrop.add(Material.DIODE_BLOCK_ON);
+        wouldDrop.add(Material.GOLD_PLATE);
+        wouldDrop.add(Material.IRON_DOOR_BLOCK); // Still Breaks
+        wouldDrop.add(Material.IRON_PLATE);
+        wouldDrop.add(Material.JACK_O_LANTERN);
+        wouldDrop.add(Material.LADDER);
+        wouldDrop.add(Material.LEVER);
+        wouldDrop.add(Material.MELON_BLOCK);
+        wouldDrop.add(Material.PUMPKIN);
+        wouldDrop.add(Material.REDSTONE);
+        wouldDrop.add(Material.REDSTONE_TORCH_OFF);
+        wouldDrop.add(Material.REDSTONE_TORCH_ON);
+        wouldDrop.add(Material.REDSTONE_WIRE);
+        wouldDrop.add(Material.RED_MUSHROOM);
+        wouldDrop.add(Material.RED_ROSE);
+        wouldDrop.add(Material.SAPLING);
+        wouldDrop.add(Material.STONE_BUTTON);
+        wouldDrop.add(Material.STONE_PLATE);
+        wouldDrop.add(Material.TORCH);
+        wouldDrop.add(Material.TRAP_DOOR);
+        wouldDrop.add(Material.TRIPWIRE_HOOK);
+        wouldDrop.add(Material.WATER_LILY);
+        wouldDrop.add(Material.WEB);
+        wouldDrop.add(Material.WOODEN_DOOR); // Still Breaks
+        wouldDrop.add(Material.WOOD_PLATE);
+        wouldDrop.add(Material.YELLOW_FLOWER);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onPistonExtend(BlockPistonExtendEvent event) {
         if (!plugin.getConfig().getBoolean("track_creative_place.enabled")) {
             return;
+        }
+        if (plugin.getConfig().getBoolean("track_creative_place.no_piston_move")) {
+            List<Block> blocks = new ArrayList<Block>();
+            for (int i = 1; i < 13; i++) {
+                blocks.add(event.getBlock().getRelative(event.getDirection(), i));
+            }
+            for (Block b : blocks) {
+                if (plugin.getCreativeBlocks().contains(b.getLocation().toString()) && wouldDrop.contains(b.getType())) {
+                    event.setCancelled(true);
+                    plugin.debug("Cancelled piston extension because one of the moved blocks would drop an item");
+                    return;
+                }
+            }
         }
         if (plugin.getCreativeBlocks().contains(event.getBlock().getLocation().toString())) {
             return;
