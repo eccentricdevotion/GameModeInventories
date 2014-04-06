fe57a5a0b6b9a2ff6747588ea31dd4dceba8e8e5
eccentric devotion <eccentricdevotion@gmail.com>
2014-04-06 22:57:32 +1200

    Bump version number (twice because I forgot last release)

--- src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesConstants.java
+++
@@ -1,9 +0,0 @@
-package me.eccentric_nz.gamemodeinventories;
-
-import org.bukkit.ChatColor;
-
-public class GameModeInventoriesConstants {
-
-    public static String MY_PLUGIN_NAME = ChatColor.GOLD + "[GameModeInventories] " + ChatColor.RESET;
-    public static String HELP = "There is no help! Just switch game modes, and your inventory/armor/xp will change.";
-}
