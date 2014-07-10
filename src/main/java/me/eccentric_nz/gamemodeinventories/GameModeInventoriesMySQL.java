7675976e750bb379fbdefaa20fda6613cfed8e06
eccentric devotion <eccentricdevotion@gmail.com>
2014-07-10 13:45:07 +1200

    Fix MySQL table create error

--- src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesMySQL.java
+++ src/main/java/me/eccentric_nz/gamemodeinventories/GameModeInventoriesMySQL.java
@@ -28,7 +28,7 @@ public class GameModeInventoriesMySQL {
             service.testConnection(connection);
             statement = connection.createStatement();
             // add inventories table
-            String queryInventories = "CREATE TABLE inventories (id int(11) NOT NULL AUTO_INCREMENT, uuid varchar(48) DEFAULT '', player varchar(24) DEFAULT '', gamemode varchar(24) DEFAULT '', inventory text, xp double, armour text, enderchest text, PRIMARY KEY (id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
+            String queryInventories = "CREATE TABLE IF NOT EXISTS inventories (id int(11) NOT NULL AUTO_INCREMENT, uuid varchar(48) DEFAULT '', player varchar(24) DEFAULT '', gamemode varchar(24) DEFAULT '', inventory text, xp double, armour text, enderchest text, PRIMARY KEY (id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
             statement.executeUpdate(queryInventories);
             // add blocks table
             String queryBlocks = "CREATE TABLE IF NOT EXISTS blocks (id int(11) NOT NULL AUTO_INCREMENT, location text, PRIMARY KEY (id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
